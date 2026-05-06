7c105cf97937fa2b413787fa0a4e6cc3e7d00e34
Jermaine Rawlins <jermaine.rawlins@gmail.com>
2026-05-06 09:16:22 -0400

    feat(infra): add DynamoDB permissions and LOOKBACK_DAYS env to scheduler

--- src/Infrastructure/stacks/scheduler-stack.ts
+++ src/Infrastructure/stacks/scheduler-stack.ts
@@ -9,26 +9,42 @@ import * as Path from 'path'
 import { lambdaFunctionIdentifier } from './base-stack'
 
 const LAMBDA_JOBS_PATH = '../../lambda/jobs/index.ts'
+const LOOKBACK_DAYS = '30'
 
 type SchedulerStackProps = StackProps & {
   handler: string
   duration: Duration
+  tableArn: string
 }
 
 class SchedulerStack extends Stack {
   constructor(scope: Construct, id: string, props: SchedulerStackProps) {
     super(scope, id, props)
 
-    const scheduler = this.buildLambdaFunction(props.handler)
+    const scheduler = this.buildLambdaFunction(props.handler, {
+      LOOKBACK_DAYS,
+    })
 
     scheduler.addToRolePolicy(
       new iam.PolicyStatement({
-        actions: [
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:PutLogEvents',
-        ],
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
         resources: ['*'],
+      }),
+    )
+
+    scheduler.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['dynamodb:Query'],
+        resources: [`${props.tableArn}/index/*`],
+      }),
+    )
+
+    scheduler.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['dynamodb:DeleteItem'],
+        resources: [props.tableArn],
       }),
     )
 
@@ -41,15 +57,14 @@ class SchedulerStack extends Stack {
 
   buildLambdaFunction(
     name: string,
-    //environment: LambdaEnvironment,
+    environment: { [key: string]: string },
   ): lambda.Function {
     return new NodejsFunction(this, lambdaFunctionIdentifier(name), {
       architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_22_X
-      ,
+      runtime: lambda.Runtime.NODEJS_22_X,
       handler: name,
       entry: Path.join(__dirname, LAMBDA_JOBS_PATH),
-      //environment,
+      environment,
     })
   }
 }
