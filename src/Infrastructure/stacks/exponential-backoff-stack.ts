f8ceaedf3f0e499acfb44a73e436c1e5b1a326d8
Jermaine Rawlins <jermaine.rawlins@gmail.com>
2026-05-06 09:16:13 -0400

    feat(infra): add SQS env var and SendMessage permission to trigger Lambda

--- src/Infrastructure/stacks/exponential-backoff-stack.ts
+++ src/Infrastructure/stacks/exponential-backoff-stack.ts
@@ -1,21 +1,23 @@
 import { Construct } from 'constructs'
-import { StackProps } from 'aws-cdk-lib'
-import { Stack } from 'aws-cdk-lib'
+import { StackProps, Stack } from 'aws-cdk-lib'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
-import { lambdaFunctionIdentifier } from './base-stack'
+import { lambdaFunctionIdentifier, MESSAGES_QUEUE_URL_PARAMETER_NAME } from './base-stack'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources'
 import * as Path from 'path'
-import { Table } from 'aws-cdk-lib/aws-dynamodb'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import { Table } from 'aws-cdk-lib/aws-dynamodb'
+import { StringParameter } from 'aws-cdk-lib/aws-ssm'
 
 const LAMBDA_STREAMS_PATH = '../../lambda/triggers/index.ts'
 const MESSAGES_TABLE_IMPORT_ID = 'MessagesTableImport'
+const MESSAGES_QUEUE_URL_PARAMETER_ID = 'TriggerMessagesQueueUrlParameter'
 
 type ExponentialBackoffStackProps = StackProps & {
   handler: string
   tableArn: string
   tableStreamArn: string
+  queueArn: string
 }
 
 class ExponentialBackoffStack extends Stack {
@@ -29,40 +31,48 @@ class ExponentialBackoffStack extends Stack {
       handler: props.handler,
       tableArn: props.tableArn,
       tableStreamArn: props.tableStreamArn,
+      queueArn: props.queueArn,
     })
   }
 
-  //// Build delete trigger
   build(buildProps: {
     handler: string
     tableArn: string
     tableStreamArn: string
+    queueArn: string
   }) {
-    //// Lambda function for delete trigger
-    ////
-    const deleteTrigger = this.buildLambdaFunction(buildProps.handler)
-    // Grant permissions to lambda function
+    const queueUrl = StringParameter.fromStringParameterAttributes(
+      this,
+      MESSAGES_QUEUE_URL_PARAMETER_ID,
+      { parameterName: MESSAGES_QUEUE_URL_PARAMETER_NAME, version: 1 },
+    ).stringValue
+
+    const deleteTrigger = this.buildLambdaFunction(buildProps.handler, {
+      MESSAGES_QUEUE_URL: queueUrl,
+    })
+
     deleteTrigger.addToRolePolicy(
       new iam.PolicyStatement({
-        actions: [
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:PutLogEvents',
-        ],
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
         resources: ['*'],
       }),
     )
 
-    // DynamoDB table for delete trigger
-    // The table must be imported using the tableArn and tableStreamArn
+    deleteTrigger.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['sqs:SendMessage'],
+        resources: [buildProps.queueArn],
+      }),
+    )
+
     const table = Table.fromTableAttributes(this, MESSAGES_TABLE_IMPORT_ID, {
       tableArn: buildProps.tableArn,
       tableStreamArn: buildProps.tableStreamArn,
     })
-    // Grant read stream access to lambda function
+
     table.grantStreamRead(deleteTrigger)
 
-    // Add event source to lambda function
     deleteTrigger.addEventSource(
       new lambdaEventSources.DynamoEventSource(table, {
         startingPosition: lambda.StartingPosition.TRIM_HORIZON,
@@ -74,14 +84,14 @@ class ExponentialBackoffStack extends Stack {
 
   buildLambdaFunction(
     name: string,
-    //environment: LambdaEnvironment,
+    environment: { [key: string]: string },
   ): lambda.Function {
     return new NodejsFunction(this, lambdaFunctionIdentifier(name), {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: name,
       entry: Path.join(__dirname, LAMBDA_STREAMS_PATH),
-      //environment,
+      environment,
     })
   }
 }
